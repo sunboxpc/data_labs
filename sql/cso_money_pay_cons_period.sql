9b34d9b93d059c09b3aa03d9d2616efbbf59bf16
sunboxpc <sunboxpc@gmail.com>
2020-03-24 16:14:06 +0300

    additional transform to close to output format

--- sql/cso_money_pay_cons_period.sql
+++ sql/cso_money_pay_cons_period.sql
@@ -1,81 +1,3 @@
-select 	CLIE_ID, SUBS_ID, ACCOUNT, NZ, CHANNEL, CHANNEL_DETAIL, UPPER(FIOPRO) as FIOPRO, UPPER(FIO_SUP) as FIO_SUP, 
-		DATE_SALE, to_date(to_char(PERIOD), 'YYYYMM') as PERIOD_SALE,SERV_NAME, RTPL_ID, TP, BRNC_ID, FILIAL, LTC, TOP147, --TCET as TSTET,
-		TOWN_ID, NP, STREET, HOUSE, KORP, APARTMENT, PHONE_ROOM, DATE_CHURN
-        --, trunc(DATE_CHURN, 'YYYYMM') as PERIOD_CHURN
-        , NAME_GRP, 1 as NEWSALE,
-		case when (DATE_CHURN is null)  then 0 else 1 end as CHURN, case when P1_AMOUNT_PAY > 0 then 1 else 0 end as PAYSTATUS, 
-		nvl(P1_PERIOD_SERV,0) as "P1_period_serv", nvl(P1_AMOUNT_SERV,0) as "P1_amount_serv", nvl(P2_PERIOD_SERV,0) as "P2_period_serv",
-		nvl(P2_AMOUNT_SERV,0) as "P2_amount_serv", nvl(P3_PERIOD_SERV,0) as "P3_period_serv", nvl(P3_AMOUNT_SERV,0) as "P3_amount_serv",
-		nvl(P4_PERIOD_SERV,0) as "P4_period_serv", nvl(P4_AMOUNT_SERV,0) as "P4_amount_serv", 
-        nvl(P1_PERIOD_EQ,0) as "P1_period_eq", nvl(P1_AMOUNT_EQ,0) as "P1_amount_eq", nvl(P2_PERIOD_EQ,0) as "P2_period_eq",
-        nvl(P2_AMOUNT_EQ,0) as "P2_amount_eq", nvl(P3_PERIOD_EQ,0) as "P3_period_eq", nvl(P3_AMOUNT_EQ,0) as "P3_amount_eq",
-		nvl(P4_PERIOD_EQ,0) as "P4_period_eq", nvl(P4_AMOUNT_EQ,0) as "P4_amount_eq",
-        nvl(P1_PERIOD_PAY,0) as "P1_period_pay", nvl(P1_AMOUNT_PAY,0) as "P1_amount_pay", nvl(P2_PERIOD_PAY,0) as "P2_period_pay", 
-        nvl(P2_AMOUNT_PAY,0) as "P2_amount_pay", nvl(P3_PERIOD_PAY,0) as "P3_period_pay", nvl(P3_AMOUNT_PAY,0) as "P3_amount_pay", 
-        nvl(P4_PERIOD_PAY,0) as "P4_period_pay", nvl(P4_AMOUNT_PAY,0) as "P4_amount_pay"
-        , nvl(round((P1_AMOUNT_PAY+P2_AMOUNT_PAY+P3_AMOUNT_PAY)/3,2),0) as AVG_PAY_3M
-        , nvl(round((P1_AMOUNT_SERV+P2_AMOUNT_SERV+P3_AMOUNT_SERV)/3,2),0) as AVG_SERV_3M
-        , nvl(round((P1_AMOUNT_EQ+P2_AMOUNT_EQ+P3_AMOUNT_EQ)/3,2),0) as AVG_EQ_3M
---       ,  as MAX_DATE
-        , case when (DATE_CHURN is null) 
-            then first_value(DATE_SALE) over(partition by 1 order by DATE_SALE desc)-DATE_SALE
-            else TO_NUMBER(DATE_CHURN-DATE_SALE) end as LIFE_TIME_script
---        , ORA_HASH(nvl(FILIAL,'') || nvl(UPPER(FIOPRO),'') || nvl(CHANNEL,''), 1e8) as KEY 
- 
-from (
-    select CLIE_ID, SUBS_ID, ACCOUNT, NZ, CHANNEL, CHANNEL_DETAIL, FIOPRO, FIO_SUP, 
-		DATE_SALE, PERIOD, SERV_NAME, RTPL_ID, TP, BRNC_ID, FILIAL, LTC, TOP147, --TCET as TSTET,
-		TOWN_ID, NP, STREET, HOUSE, KORP, APARTMENT, PHONE_ROOM    
-    from crs.cso_connects t
-    where t.PERIOD between 202001 and 202003 and CHANNEL = 'Активный канал'
-    )
-left join (
-    select CLIE_ID, SUBS_ID, NAME_GRP, DATE_CHURN
-    from crs.cso_disconnects 
-    where exists (	
-        select * 
-        from crs.cso_connects
-		where crs.cso_connects.SUBS_ID = crs.cso_disconnects.SUBS_ID
-                and crs.cso_connects.CLIE_ID = crs.cso_disconnects.CLIE_ID
-                and  crs.cso_connects.DATE_SALE <= crs.cso_disconnects.DATE_CHURN
-				) 
-            and PERIOD between 202001 and 202012
-    ) c using (CLIE_ID, SUBS_ID)
-left join (
-	select PERIOD_CONNECT, CLIE_ID, SUBS_ID, nvl(P1_PERIOD_SERV,0) as P1_PERIOD_SERV
-        , nvl(P1_AMOUNT_SERV,0) as P1_AMOUNT_SERV, nvl(P2_PERIOD_SERV,0) as P2_PERIOD_SERV
-        , nvl(P2_AMOUNT_SERV,0) as P2_AMOUNT_SERV, nvl(P3_PERIOD_SERV,0) as P3_PERIOD_SERV
-        , nvl(P3_AMOUNT_SERV,0) as P3_AMOUNT_SERV,nvl(P4_PERIOD_SERV,0) as P4_PERIOD_SERV
-        , nvl(P4_AMOUNT_SERV,0) as P4_AMOUNT_SERV     
-	from (
-		select PERIOD_CONNECT, CLIE_ID, SUBS_ID, PERIOD_SERV, SUMM_SERV as AMOUNT_SERV,
-		row_number() over(partition by CLIE_ID, SUBS_ID order by PERIOD_SERV) as PERIOD_SERV_ID
-		from crs.cso_money_basic_services
-		where PERIOD_CONNECT between 202001 and 202012 and (PERIOD_SERV - PERIOD_CONNECT-1) between 0 and 3
-	--	and rownum <=100
-		) 
-	pivot ( sum(AMOUNT_SERV) as AMOUNT_SERV, max(PERIOD_SERV) as PERIOD_SERV
-			for PERIOD_SERV_ID in (1 as P1,2 as P2,3 as P3,4 as P4)
-			)
-	) s using (CLIE_ID, SUBS_ID)
-left join (
-		select 	PERIOD_CONNECT, CLIE_ID, nvl(P1_PERIOD_EQ,0) as P1_PERIOD_EQ
-        , nvl(P1_AMOUNT_EQ,0) as P1_AMOUNT_EQ, nvl(P2_PERIOD_EQ,0) as P2_PERIOD_EQ
-        , nvl(P2_AMOUNT_EQ,0) as P2_AMOUNT_EQ, nvl(P3_PERIOD_EQ,0) as P3_PERIOD_EQ
-        , nvl(P3_AMOUNT_EQ,0) as P3_AMOUNT_EQ,nvl(P4_PERIOD_EQ,0) as P4_PERIOD_EQ
-        , nvl(P4_AMOUNT_EQ,0) as P4_AMOUNT_EQ       
-	from (
-		select PERIOD_CONNECT, CLIE_ID, PERIOD_EQ, SUM_EQ,
-		row_number() over(partition by CLIE_ID order by PERIOD_EQ) as PERIOD_EQ_ID
-		from crs.cso_money_equipments 
-		where PERIOD_CONNECT between 202001 and 202012 and (PERIOD_EQ - PERIOD_CONNECT -1 ) between 0 and 3
-	--	and rownum <10
-		)
-	pivot ( sum(SUM_EQ) as AMOUNT_EQ, max(PERIOD_EQ) as PERIOD_EQ
-			for PERIOD_EQ_ID in (1 as P1,2 as P2,3 as P3,4 as P4)
-		)	
-	) e using (CLIE_ID)
-left join (
 select  CLIE_ID, nvl(P1_PERIOD_PAY,0) as P1_PERIOD_PAY 
         , nvl(P1_AMOUNT_PAY, 0) as P1_AMOUNT_PAY, nvl(P2_PERIOD_PAY,0) as P2_PERIOD_PAY
         , nvl(P2_AMOUNT_PAY,0) as P2_AMOUNT_PAY, nvl(P3_PERIOD_PAY,0) as P3_PERIOD_PAY
@@ -139,7 +61,7 @@ from (
 --              PAY_ID = 1
                    
 		)
---    where CLIE_ID = 41196163
+    where CLIE_ID = 41196163
  
 )
 	pivot ( sum(AMOUNT_PAY) as AMOUNT_PAY, max(PERIOD_PAY) as PERIOD_PAY
@@ -177,7 +99,6 @@ where -- MIN_PERIOD_PAY - CONS_PERIOD_PAY_GRP = 0 --условие выбора только первых
         прошло меньше 3 периодов (чего по бизнес-логике быть не должно), то берем платежи до следующего подключения
 */
 
-) p using (CLIE_ID)
-order by DATE_SALE
---FETCH NEXT 100 ROWS ONLY
- 
+order by CLIE_ID desc, PERIOD_CONNECT    
+--    FETCH NEXT 100 ROWS ONLY    
+ --   group by PERIOD_CONNECT, CLIE_ID, PERIOD_PAY, AMOUNT_PAY 
